5af29209a516c2029cfe8c7fe79880fff4b6a60e
Rajparmarrp <59826542+Rajparmarrp@users.noreply.github.com>
2020-07-04 21:16:37 +0530

    Add files via upload

---
+++ RouteConfig.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace Udaipur_E_Marketing
+{
+    public class RouteConfig
+    {
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                 
+               //defaults: new { controller = "User", action = "SignUp", id = UrlParameter.Optional }
+               //defaults: new { controller = "User", action = "ViewAllAds", id = UrlParameter.Optional }
+               defaults: new { controller = "Admin", action = "Login", id = UrlParameter.Optional }
+              // defaults: new { controller = "Admin", action = "ViewCategory", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
